f4a510d46eb75089c877ca7b4553f00e5cb8cbd2
liangtiecheng <>
2022-04-21 14:04:23 +0800

    update

--- content-script.js
+++ content-script.js
@@ -12,6 +12,9 @@ const HOST = {
   MAIMAI: 'maimai.cn',
   ZHIHU: 'www.zhihu.com',
   ZHIHU_ZHUANLAN: 'zhuanlan.zhihu.com',
+  JIANSHU: 'www.jianshu.com',
+  INTERVIEW: 'vue3js.cn',
+  SHANGGUIGU: 'www.atguigu.com',
 }
 
 const handler = {
@@ -21,6 +24,20 @@ const handler = {
   },
   [HOST.BILIBILI]: () => {
     injectCSS('bilibili-styles.css')
+    document.querySelector('#app').style = 'background-color:black;'
+    document.querySelector('.v-wrap').style = 'background-color:black;'
+    const ul = document.querySelector('.bilibili-player-video-btn-speed-menu')
+    const createSpeed = (speed) => {
+      const element = document.createElement('li')
+      element.classList.add('bilibili-player-video-btn-speed-menu-list')
+      element.dataset.value = speed
+      element.innerText = `${speed}x`
+      ul.insertBefore(element, ul.firstChild)
+    }
+    createSpeed(2.5)
+    setTimeout(() => {
+      createSpeed(3.0)
+    }, 3000)
   },
   [HOST.MAIMAI]: () => {
     injectCSS('maimai-styles.css')
@@ -33,13 +50,25 @@ const handler = {
     const callback = document.querySelector('.Modal-closeButton').click()
     listenerNodeInsert('body', 'Modal-wrapper', callback)
   },
+  [HOST.JIANSHU]: () => {
+    injectCSS('jianshu-styles.css')
+  },
+  [HOST.INTERVIEW]: () => {
+    injectCSS('interview-styles.css')
+    setInterval(() => {
+      document.title = ''
+    }, 3000)
+  },
+  [HOST.SHANGGUIGU]: () => {
+    injectCSS('atguigu-styles.css')
+  },
 }
 
 function start() {
   const host = window.location.host
+  console.log('==> content-script', host)
   const exec = handler[host]
   exec()
-  console.log('==> content-script', host)
 }
 
 /**
@@ -75,7 +104,7 @@ function listenerNodeInsert(listener, name, callback) {
     console.log(event)
     const { id, classList } = event.target
     if (id === name || (classList && classList.value.indexOf(name) !== -1)) {
-      console.log('==> exec callback', name);
+      console.log('==> exec callback', name)
       callback()
     }
   })
