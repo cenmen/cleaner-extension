73080b5e06c65c6a25e3bbefbe17c9a3673a0558
liangtiecheng <liangtiecheng@jianke.com>
2022-09-16 15:53:00 +0800

    'nestjs广告清理

--- content-script.js
+++ content-script.js
@@ -1,9 +1,9 @@
 function injectCSS(file) {
-  const link = document.createElement('link')
-  link.href = chrome.runtime.getURL(file)
-  link.type = 'text/css'
-  link.rel = 'stylesheet'
-  document.querySelector('head').appendChild(link)
+  const link = document.createElement('link');
+  link.href = chrome.runtime.getURL(file);
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.querySelector('head').appendChild(link);
 }
 
 const HOST = {
@@ -15,60 +15,68 @@ const HOST = {
   JIANSHU: 'www.jianshu.com',
   INTERVIEW: 'vue3js.cn',
   SHANGGUIGU: 'www.atguigu.com',
-}
+  BIGERFE: 'bigerfe.com',
+  NESTJS: 'docs.nestjs.com',
+};
 
 const handler = {
   [HOST.CSDN]: () => {
-    injectCSS('csdn-styles.css')
-    autoRemoveNodeBySelector('body', 'passport-login-container')
+    injectCSS('/styles/csdn-styles.css');
+    autoRemoveNodeBySelector('body', 'passport-login-container');
   },
   [HOST.BILIBILI]: () => {
-    injectCSS('bilibili-styles.css')
-    document.querySelector('#app').style = 'background-color:black;'
-    document.querySelector('.v-wrap').style = 'background-color:black;'
-    const ul = document.querySelector('.bilibili-player-video-btn-speed-menu')
-    const createSpeed = (speed) => {
-      const element = document.createElement('li')
-      element.classList.add('bilibili-player-video-btn-speed-menu-list')
-      element.dataset.value = speed
-      element.innerText = `${speed}x`
-      ul.insertBefore(element, ul.firstChild)
-    }
-    createSpeed(2.5)
+    injectCSS('/styles/bilibili-styles.css');
+    document.querySelector('#app').style = 'background-color:black;';
+    document.querySelector('.v-wrap').style = 'background-color:black;';
+    const ul = document.querySelector('.bilibili-player-video-btn-speed-menu');
+    const createSpeed = speed => {
+      const element = document.createElement('li');
+      element.classList.add('bilibili-player-video-btn-speed-menu-list');
+      element.dataset.value = speed;
+      element.innerText = `${speed}x`;
+      ul.insertBefore(element, ul.firstChild);
+    };
+    createSpeed(2.5);
     setTimeout(() => {
-      createSpeed(3.0)
-    }, 3000)
+      createSpeed(3.0);
+    }, 3000);
   },
   [HOST.MAIMAI]: () => {
-    injectCSS('maimai-styles.css')
+    injectCSS('/styles/maimai-styles.css');
   },
   [HOST.ZHIHU]: () => {
-    const callback = document.querySelector('.Modal-closeButton').click()
-    listenerNodeInsert('body', 'Modal-wrapper', callback)
+    const callback = document.querySelector('.Modal-closeButton').click();
+    listenerNodeInsert('body', 'Modal-wrapper', callback);
   },
   [HOST.ZHIHU_ZHUANLAN]: () => {
-    const callback = document.querySelector('.Modal-closeButton').click()
-    listenerNodeInsert('body', 'Modal-wrapper', callback)
+    const callback = document.querySelector('.Modal-closeButton').click();
+    listenerNodeInsert('body', 'Modal-wrapper', callback);
   },
   [HOST.JIANSHU]: () => {
-    injectCSS('jianshu-styles.css')
+    injectCSS('/styles/jianshu-styles.css');
   },
   [HOST.INTERVIEW]: () => {
-    injectCSS('interview-styles.css')
+    injectCSS('/styles/interview-styles.css');
     setInterval(() => {
-      document.title = ''
-    }, 3000)
+      document.title = '';
+    }, 3000);
   },
   [HOST.SHANGGUIGU]: () => {
-    injectCSS('atguigu-styles.css')
+    injectCSS('/styles/atguigu-styles.css');
   },
-}
+  [HOST.BIGERFE]: () => {
+    injectCSS('/styles/bigerfe-styles.css');
+  },
+  [HOST.NESTJS]: () => {
+    injectCSS('/styles/nestjs-styles.css');
+  },
+};
 
 function start() {
-  const host = window.location.host
-  console.log('==> content-script', host)
-  const exec = handler[host]
-  exec()
+  const host = window.location.host;
+  console.log('==> content-script', host);
+  const exec = handler[host];
+  exec();
 }
 
 /**
@@ -78,17 +86,17 @@ function start() {
  * @return {*}
  */
 function autoRemoveNodeBySelector(listener, name) {
-  if (!listener && !name) return
-  const _listener = document.querySelector(listener)
-  _listener.addEventListener('DOMNodeInserted', (event) => {
-    console.log(event)
-    const { id, classList } = event.target
+  if (!listener && !name) return;
+  const _listener = document.querySelector(listener);
+  _listener.addEventListener('DOMNodeInserted', event => {
+    console.log(event);
+    const { id, classList } = event.target;
     if (id === name || (classList && classList.value.indexOf(name) !== -1)) {
-      console.log('==> removeChild', listener, name)
-      const check = document.querySelector(`#${name}`) || document.querySelector(`.${name}`)
-      check && _listener.removeChild(event.target)
+      console.log('==> removeChild', listener, name);
+      const check = document.querySelector(`#${name}`) || document.querySelector(`.${name}`);
+      check && _listener.removeChild(event.target);
     }
-  })
+  });
 }
 
 /**
@@ -98,16 +106,16 @@ function autoRemoveNodeBySelector(listener, name) {
  * @return {*}
  */
 function listenerNodeInsert(listener, name, callback) {
-  if (!listener && !name && !callback) return
-  const _listener = document.querySelector(listener)
-  _listener.addEventListener('DOMNodeInserted', (event) => {
-    console.log(event)
-    const { id, classList } = event.target
+  if (!listener && !name && !callback) return;
+  const _listener = document.querySelector(listener);
+  _listener.addEventListener('DOMNodeInserted', event => {
+    console.log(event);
+    const { id, classList } = event.target;
     if (id === name || (classList && classList.value.indexOf(name) !== -1)) {
-      console.log('==> exec callback', name)
-      callback()
+      console.log('==> exec callback', name);
+      callback();
     }
-  })
+  });
 }
 
-start()
+start();
